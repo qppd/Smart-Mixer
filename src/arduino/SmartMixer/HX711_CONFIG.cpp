ddebad2b7123a231476438a24899445d81847fdc
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2026-02-07 11:52:57 +0800

    Update HX711 load cell pins to DT=4, SCK=3

---
+++ src/arduino/SmartMixer/HX711_CONFIG.cpp
@@ -0,0 +1,28 @@
+#include "HX711_CONFIG.h"
+
+HX711 scale;
+
+void initLOADCELL() {
+  scale.begin(HX711_DT, HX711_SCK);
+
+  scale.set_scale(22500.3f);  // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();              // reset the scale to 0
+
+  Serial.println("HX711 Tare successful");
+}
+
+float getLOADCELLWeight() {
+  float weight = scale.get_units(10);
+  if (weight < 0)
+    return 0;
+  else
+    return weight;
+}
+
+void operateHX711sleep() {
+  scale.power_down();
+}
+
+void operateHX711wake() {
+  scale.power_up();
+}
