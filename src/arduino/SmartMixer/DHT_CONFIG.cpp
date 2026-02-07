12216c2bce2f21330082f44dfe4b956f436a06fa
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2026-02-07 11:50:00 +0800

    Add DHT22 sensor configuration and functions
    - Update DHT_CONFIG.h to use DHT22 with proper includes and function declarations
    - Create DHT_CONFIG.cpp with initDHT, getDHTTemperature, and getDHTHumidity functions
    - Functions include error handling returning -1 for invalid readings

---
+++ src/arduino/SmartMixer/DHT_CONFIG.cpp
@@ -0,0 +1,30 @@
+#include "DHT_CONFIG.h"
+
+DHT dht(DHT_PIN, DHTTYPE);
+
+void initDHT() {
+  dht.begin();
+}
+
+/*
+  Function/Method for getting DHT temperature
+*/
+float getDHTTemperature(boolean isFarenheit) {
+  float temperature;
+  if (isFarenheit) {
+    temperature = dht.readTemperature(true);
+  } else {
+    temperature = dht.readTemperature();
+  }
+  if (isnan(temperature)) return -1;
+  else return temperature;
+}
+
+/*
+  Function/Method for getting DHT humidity
+*/
+float getDHTHumidity() {
+  float humidity = dht.readHumidity();
+  if (isnan(humidity)) return -1;
+  else return humidity;
+}
--- src/arduino/SmartMixer/DHT_CONFIG.h
+++ src/arduino/SmartMixer/DHT_CONFIG.h
@@ -2,8 +2,26 @@
 #define DHT_CONFIG_H
 
 #include "PINS_CONFIG.h"
+#include "DHT.h"
 
-// DHT sensor type configuration
-#define DHT_TYPE DHT11  // or DHT22
+// DHT sensor configuration
+#define DHTTYPE DHT22  // DHT 22 (AM2302), AM2321
+
+extern DHT dht;
+
+//-----------------------------------------------------------------
+//FUNCTION FOR INITIALIZING DHT SENSOR-----------------------------
+//-----------------------------------------------------------------
+void initDHT();
+
+//-----------------------------------------------------------------
+//FUNCTION FOR GETTING DHT TEMPERATURE----------------------------
+//-----------------------------------------------------------------
+float getDHTTemperature(boolean isFarenheit);
+
+//-----------------------------------------------------------------
+//FUNCTION FOR GETTING DHT HUMIDITY-------------------------------
+//-----------------------------------------------------------------
+float getDHTHumidity();
 
 #endif
