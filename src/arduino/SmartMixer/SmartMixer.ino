acb3c8de4df4441c6daecbcd5235c456882c9f1e
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2026-02-07 12:18:49 +0800

    Update continuous monitoring interval to 1 second
    - Changed sensor reading interval from 2 seconds to 1 second
    - Faster updates for continuous monitoring modes
    - More responsive real-time sensor feedback

--- src/arduino/SmartMixer/SmartMixer.ino
+++ src/arduino/SmartMixer/SmartMixer.ino
@@ -4,12 +4,18 @@
 #include "HX711_CONFIG.h"
 #include "RELAY_CONFIG.h"
 
+// Continuous monitoring flags for each sensor
+bool continuousTemp = false;
+bool continuousHum = false;
+bool continuousPH = false;
+bool continuousWeight = false;
+
 void setup() {
     Serial.begin(9600);
     initLCD();
-    //initDHT();
-    //initLOADCELL();
-    //initRELAY();
+    initDHT();
+    initLOADCELL();
+    initRELAY();
 
     setLCDText("System Ready", 0, 0);
     Serial.println("Smart Mixer System Ready");
@@ -17,16 +23,57 @@ void setup() {
     Serial.println("get-temp, get-hum, relay1_on, relay1_off");
     Serial.println("relay2_on, relay2_off, get-ph, get-weight");
     Serial.println("set-lcd:<text>, stop");
+    Serial.println("start-temp-continuous, stop-temp-continuous");
+    Serial.println("start-hum-continuous, stop-hum-continuous");
+    Serial.println("start-ph-continuous, stop-ph-continuous");
+    Serial.println("start-weight-continuous, stop-weight-continuous");
 }
 
 void loop() {
-    // Standby mode - just show ready message
+    // Continuous sensor readings based on individual flags
+    static unsigned long lastRead = 0;
+    if (millis() - lastRead > 1000) {  // Read every 1 second
+        if (continuousTemp) {
+            float temp = getDHTTemperature(false);
+            Serial.print("Temp: ");
+            Serial.print(temp);
+            Serial.println(" °C");
+            setLCDText("Temp: " + String(temp) + "C", 0, 0);
+        }
+        if (continuousHum) {
+            float hum = getDHTHumidity();
+            Serial.print("Humidity: ");
+            Serial.print(hum);
+            Serial.println(" %");
+            setLCDText("Hum: " + String(hum) + "%", 0, 1);
+        }
+        if (continuousPH) {
+            float ph = getPHValue();
+            Serial.print("pH: ");
+            Serial.println(ph);
+            setLCDText("pH: " + String(ph), 0, 0);
+        }
+        if (continuousWeight) {
+            float weight = getLOADCELLWeight();
+            Serial.print("Weight: ");
+            Serial.print(weight);
+            Serial.println(" units");
+            setLCDText("Weight: " + String(weight), 0, 1);
+        }
+        lastRead = millis();
+    }
+
+    // Standby mode display when no continuous monitoring is active
     static bool standbyShown = false;
-    if (!standbyShown) {
-        setLCDText("Standby Mode", 0, 0);
-        clearLCD();
-        setLCDText("Standby Mode", 0, 0);
-        standbyShown = true;
+    if (!continuousTemp && !continuousHum && !continuousPH && !continuousWeight) {
+        if (!standbyShown) {
+            setLCDText("Standby Mode", 0, 0);
+            clearLCD();
+            setLCDText("Standby Mode", 0, 0);
+            standbyShown = true;
+        }
+    } else {
+        standbyShown = false; // Reset when any continuous mode is active
     }
 
     // Process serial commands
@@ -40,8 +87,42 @@ void loop() {
             while (true); // Stop execution
         }
 
-        // Process commands and show results
-        if (command == "get-temp") {
+        // Continuous monitoring commands
+        else if (command == "start-temp-continuous") {
+            continuousTemp = true;
+            Serial.println("Temperature continuous monitoring started");
+        }
+        else if (command == "stop-temp-continuous") {
+            continuousTemp = false;
+            Serial.println("Temperature continuous monitoring stopped");
+        }
+        else if (command == "start-hum-continuous") {
+            continuousHum = true;
+            Serial.println("Humidity continuous monitoring started");
+        }
+        else if (command == "stop-hum-continuous") {
+            continuousHum = false;
+            Serial.println("Humidity continuous monitoring stopped");
+        }
+        else if (command == "start-ph-continuous") {
+            continuousPH = true;
+            Serial.println("pH continuous monitoring started");
+        }
+        else if (command == "stop-ph-continuous") {
+            continuousPH = false;
+            Serial.println("pH continuous monitoring stopped");
+        }
+        else if (command == "start-weight-continuous") {
+            continuousWeight = true;
+            Serial.println("Weight continuous monitoring started");
+        }
+        else if (command == "stop-weight-continuous") {
+            continuousWeight = false;
+            Serial.println("Weight continuous monitoring stopped");
+        }
+
+        // Process other commands and show results
+        else if (command == "get-temp") {
             float temp = getDHTTemperature(false);
             Serial.print("Temperature: ");
             Serial.print(temp);
